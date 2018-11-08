56f4b5aa617a2649469c92b204c4ef5f3e31cc79
adeeedo <36483740+adeeedo@users.noreply.github.com>
2018-11-08 18:03:28 +0300

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,5 +1,4 @@
-
-
+#!/usr/bin/env python3
 import psycopg2
 
 DBNAME = "news"
@@ -7,28 +6,36 @@ DBNAME = "news"
 def results():
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
-  c.execute(" select articles.title , finalview.count from articles , finalview where articles.slug=finalview.cut order by count desc limit 3;")
-  posts = c.fetchall()
   print("\n")
-  print("first question : ")
-  print("\n")
-  print( posts)
-  print("\n")
-  c1= db.cursor()
-  c1.execute(" select distinct(authorsids.name) , summedviews.author , summedviews.sum from authorsids , summedviews where authorsids.author=summedviews.author order by sum desc;")
-  posts1 = c1.fetchall()
-  print("\n")
-  print("second question : ")
-  print("\n")
-  print(posts1)
-  c2= db.cursor()
-  c2.execute("  select date, percent from percentage where percent > 1;")
-  posts2 = c2.fetchall()
-  print("\n")
-  print("third question : ")
-  print("\n")
-  print(posts2)
-  db.close()
+  print("What are the most popular three articles of all time? \n")
+  c.execute( "select  articles.title, cast(finalview.count as varchar)  from articles , finalview where articles.slug=finalview.cut order by count desc limit 3;")
+  row = c.fetchall()
+  for r in row:
+    print("\"%s \" - %s views" %(r))
 
+
+
+  c1= db.cursor()
+  print("\n")
+  print("Who are the most popular article authors of all time? ")
+  print("\n")
+  c1.execute(" select distinct(authorsids.name) , cast(summedviews.sum as varchar) from authorsids , summedviews where authorsids.author=summedviews.author order by sum desc;")
+
+  row1 = c1.fetchall()
+  for r1 in row1:
+    print("\"%s\" - %s views" %(r1))
+
+
+  c2= db.cursor()
+  print("\n")
+  print("On which days did more than 1% of requests lead to errors? ")
+  print("\n")
+  c2.execute(" select FORMAT('%s  - %s %%', date , cast(percent as varchar))  from percentage where percent > 1;")
+  
+  row2 = c2.fetchall()
+  for r2 in row2:
+    print("\"%s\"" %(r2))
+
+  db.close()
 results()
 
